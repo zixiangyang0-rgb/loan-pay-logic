c1ecb3a82975bbe54d056472b801930ddfe41b0a
zixiangyang0-rgb <zixiangyang0@gmail.com>
2026-05-12 22:46:58 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="zh">
+      <body>{children}</body>
+    </html>
+  );
+}
